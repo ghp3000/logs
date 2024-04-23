3987ee6f331091b2c2f5d6dcbf065d6b75210448
ghp3000 <ghp3000@163.com>
2024-04-23 13:34:27 +0800

    新增默认实例

--- fileAdapter.go
+++ fileAdapter.go
@@ -17,11 +17,11 @@ import (
 	"time"
 )
 
-type rollingType uint8
+type RollingType uint8
 
 const (
-	RollingDaily rollingType = 0 //按日期分割文件
-	RollingFile  rollingType = 1 //按文件大小分割文件
+	RollingDaily RollingType = 0 //按日期分割文件
+	RollingFile  RollingType = 1 //按文件大小分割文件
 
 	dateformatDay = "20060102"
 )
@@ -39,7 +39,7 @@ type FileConfig struct {
 	FileName    string
 	FileMaxSize int64
 	FileMaxNum  int
-	RollType    rollingType
+	RollType    RollingType
 	Gzip        bool
 }
 
@@ -180,7 +180,7 @@ type fileStore struct {
 	fileName    string      //日志文件名
 	maxSize     int64       //日志文件的最大大小
 	fileSize    int64       //记录当前日志文件的大小
-	rollType    rollingType //日志的滚动模式,按日期或按文件大小
+	rollType    RollingType //日志的滚动模式,按日期或按文件大小
 	tomorSecond int64       //日志文件的下一天的时间戳
 	isFileWell  bool        //文件是否正常
 	maxFileNum  int         //日志文件最大数量
